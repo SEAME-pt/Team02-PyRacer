62637525519c57f778580c12565d440abea945e7
Rui Pires <ruipedro.pires999@gmail.com>
2025-01-16 13:46:47 +0000

    feat: add blinker functionality to Xbox controller events

--- XboxController/src/main.cpp
+++ XboxController/src/main.cpp
@@ -11,19 +11,42 @@ int main(void)
 
     XboxController player(session);
     size_t axis;
+    size_t button;
 
     auto pubThrottle =
         session.declare_publisher(KeyExpr("seame/car/1/throttle"));
     auto pubDirection =
         session.declare_publisher(KeyExpr("seame/car/1/direction"));
+    auto pubLights = session.declare_publisher(KeyExpr("seame/car/1/lights"));
 
     while (player.readEvent() == 0)
     {
         switch (player.event.type)
         {
             case JS_EVENT_BUTTON:
-                //  std::cout << "BUTTON" << std::endl;
+            {
+                button = player.event.number;
+                switch (button)
+                {
+                    case BUTTON_RB:
+                    {
+                        bool rightBlinker = true;
+                        pubLights.put(std::to_string(rightBlinker).c_str());
+                        std::cout << "RightBlinker" << std::endl;
+                        break;
+                    }
+                    case BUTTON_LB:
+                    {
+                        bool leftBlinker = true;
+                        pubLights.put(std::to_string(leftBlinker).c_str());
+                        std::cout << "LeftBlinker" << std::endl;
+                        break;
+                    }
+                    default:
+                        break;
+                }
                 break;
+            }
             case JS_EVENT_AXIS:
             {
                 axis = player.getAxisState();
@@ -34,6 +57,7 @@ int main(void)
                         int speed = -player.axes[axis]->y * 100 / 32767;
                         std::string speedStr = std::to_string(speed);
                         pubThrottle.put(speedStr.c_str());
+                        std::cout << "Speed" << std::endl;
                         break;
                     }
                     case (AXIS_RIGHT_STICK):
@@ -42,6 +66,7 @@ int main(void)
                             90 + player.axes[axis]->x * 90 / 32767;
                         std::string dirStr = std::to_string(direction);
                         pubDirection.put(dirStr.c_str());
+                        std::cout << "Direction" << std::endl;
                         break;
                     }
                     default:
