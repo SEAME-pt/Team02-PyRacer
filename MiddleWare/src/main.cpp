1c3015cf45b9d45a7d54840f6a71a8b66074f2ac
Rui Pires <ruipedro.pires999@gmail.com>
2025-01-22 17:50:14 +0000

    Remove commented-out code in main.cpp to improve clarity and maintainability

--- MiddleWare/src/main.cpp
+++ MiddleWare/src/main.cpp
@@ -25,39 +25,6 @@ int main(int argc, char** argv)
     std::thread signalsThread(&Signals::run, &allSigs);
     batteryThread.join();
     signalsThread.join();
-
-
-    // while(1)
-    // {
-    //     usleep(10000);
-    //     int buffer = this->canBus->checkReceive();
-    //     if (buffer != -1) {
-    //         this->canBus->readMessage(buffer);
-    //     }
-
-    //     usleep(1000); // Add a small delay to avoid busy waiting
-
-    //     double status = this->batteryINA->readVoltage(0x02);
-    //     std::cout << "Battery: " << status << std::endl;
-
-    //     uint8_t tx[8];
-    //     memcpy(tx, &status, sizeof(status));
-    //     this->canBus->writeMessage(0x02, tx, sizeof(tx));
-    // }
-
-    // while (1)
-    // {
-    //     else if (frame.can_id == 0x02)
-    //     {
-    //         int battery;
-    //         memcpy(&battery, frame.data, sizeof(int));
-    //         battery                 = ntohl(battery);
-    //         std::string battery_str = std::to_string(battery);
-
-    //         printf("Publishing battery: '%d\n", battery);
-    //         pubBattery.put(battery_str.c_str());
-    //     }
-    //     usleep(10);
-    // }
+    
     return 0;
 }
